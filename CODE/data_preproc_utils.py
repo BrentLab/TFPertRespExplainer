fixed errors in data_preproc_utils.py, preprocess_human_data.py, and optimized shap+/- computation in visualization_utils.py

CODE/data_preproc_utils.py
@@ -165,7 +165,9 @@ def get_onehot_dna_sequence_slim(regdna_bed, genome_fa, tss_df):
         strand = seq_info['strand']
         start_pos = seq_info['start']
         end_pos = seq_info['end'] 
-        tss_pos = tss_df.loc[tss_df['name'] == s.id, 'start'].iloc[0]
+
+        name = s.id.split(":")[0]
+        tss_pos = tss_df.loc[tss_df['name'] == name, 'start'].iloc[0]
         
         if strand == '+':
             rel_dists = np.arange(start_pos, end_pos, dtype=int) - tss_pos
@@ -182,7 +184,7 @@ def get_onehot_dna_sequence_slim(regdna_bed, genome_fa, tss_df):
         if len(rel_dists) != len(alphabet_idx):
             print(s.id)
 
-        gene_idx = genes.index(s.id)
+        gene_idx = genes.index(name)
         tmp_df = pd.DataFrame({
             'gene_idx': [gene_idx] * len(rel_dists),
             'rel_dist': rel_dists,
@@ -209,7 +211,7 @@ def get_nt_frequency(regdna_bed, genome_fa, genes):
     regdna_bed = regdna_bed.getfasta(fi=genome_fa, name=True)
 
     for s in load_fasta(regdna_bed.seqfn):
-        gene_idx = genes.index(s.id)
+        gene_idx = genes.index(s.id.split(":")[0])
         seq_len = len(s.seq)
         seq = str(s.seq)
         